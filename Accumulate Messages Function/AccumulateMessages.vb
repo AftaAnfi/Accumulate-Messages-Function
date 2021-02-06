0662aa1e89d440186ee42006f4591c01fb8b7788
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-02-06 15:31:22 -0700

    Finished Accumulate Messages Function

---
+++ Accumulate Messages Function/AccumulateMessages.vb
@@ -0,0 +1,52 @@
+﻿Option Strict On
+Option Explicit On
+
+'Aftanom Anfilofieff
+'RCET0265
+'Spring 2021
+'Accumulate Message Function
+'https://github.com/AftaAnfi/Accumulate-Messages-Function.git
+
+Module Module1
+
+    Sub Main()
+
+        'add ooof to total user messages 
+        UserMessages("Ooof", False)
+
+        'add another ooof to total messages and show in console
+        Console.Write(UserMessages("Ooof2", False))
+
+        'reset messages
+        Console.Write(UserMessages("", True))
+
+        'add another ooof to total messages and show in console (only one due to messages being cleared)
+        Console.Write(UserMessages("Ooof3", False))
+
+
+        Console.ReadLine()
+
+    End Sub
+
+    'Function to add messages or clear messages
+    Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
+
+        'create a string variable that's lifetime is the program
+        Static messages As String
+
+
+        'add the message into the total messages if it's not blank
+        If newMessage <> "" Then
+            messages &= (newMessage & vbNewLine)
+        End If
+
+        'if clear is true set messages to empty
+        If clear Then
+            messages = ""
+        End If
+
+        Return messages
+
+    End Function
+
+End Module
