4954482bef42c3bd7ceeac2964512ac6d978a614
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-02-06 14:54:52 -0700

    Added Header and Options to assignment

--- Accumulate Messages Function/Module1.vb
+++ Accumulate Messages Function/Module1.vb
@@ -1,6 +1,12 @@
 ﻿Option Strict On
 Option Explicit On
 
+'Aftanom Anfilofieff
+'RCET0265
+'Spring 2021
+'Accumulate Message Function
+'https://github.com/AftaAnfi/Accumulate-Messages-Function.git
+
 Module Module1
 
     Sub Main()
